e9afbc1f8127c8e5973e46a6119e696ebc4c66af
Anderson Conceição <andersonconceiicao@gmail.com>
2022-10-07 20:01:30 -0300

    feat: added package for tests

--- src/ApiGithubIntegration.CLI/Interfaces/IGithubServices.cs
+++ src/ApiGithubIntegration.CLI/Interfaces/IGithubServices.cs
@@ -1,10 +1,8 @@
 using cli_api_github_integration.Resources;
-using Refit;
 
 namespace cli_api_github_integration.Interfaces;
 
-public interface IGithubServices
+public interface IGithibServices
 {
-    [Get("/users/{userName}/repos")]
-    Task<IEnumerable<GitHubResponse>> GetUserRepositories(string userName);
+    Task<IEnumerable<GitHubResponse>> Get(string username);
 }
---
+++ src/ApiGithubIntegration.CLI/Services/GithubServices.cs
@@ -0,0 +1,6 @@
+namespace cli_api_github_integration.Services;
+
+public class GithubServices
+{
+    
+}
