1fc0025fdfad0bdaf686dd09a545d00e9f45b733
Anderson Conceição <andersonconceiicao@gmail.com>
2022-10-07 22:49:33 -0300

    feat: added new test. Coverage 59% at now

--- src/ApiGithubIntegration.UnitTests/Services/GithubServicesTests.cs
+++ src/ApiGithubIntegration.UnitTests/Services/GithubServicesTests.cs
@@ -4,27 +4,59 @@ using cli_api_github_integration.Resources;
 using cli_api_github_integration.Services;
 using FluentAssertions;
 using Moq;
+using NSubstitute;
+using Refit;
 
 namespace ApiGithubIntegration.UnitTests.Services;
 
 public class GithubServicesTests
 {
-    
+    private readonly IGithubSearch _githubSearch;
+
+    public GithubServicesTests()
+    {
+        var http = new HttpClient
+        {
+            BaseAddress = new Uri("https://api.github.com"),
+            DefaultRequestHeaders = { { "User-Agent", "Refit" } }
+        };
+        _githubSearch = RestService.For<IGithubSearch>(http);
+    }
+
     [Fact]
     public async Task GetRepos_ShouldReturnSuccess_WhenUserExists()
     {
         // Arrange
-        string userName = "andd33rson";
-        var sut = new Mock<IGithubServices>();
-
-        sut.Setup(m => m.GetRepos(userName))
-            .ReturnsAsync(new Response<IEnumerable<GitHubResponse>>(new List<GitHubResponse>(), true));
-
+        string userName = "andd3rson";
+        var sut = new GithubServices(_githubSearch);
         // Action
-        var result = await sut.Object.GetRepos(userName);
-
+        var result = await sut.GetRepos(userName);
         //Assert
         result.IsSuccess.Should().BeTrue();
-
+        result.Data.Should().BeOfType<List<GitHubResponse>>();
     }
- }
+
+    [Fact]
+    public async Task GetRepos_ShouldReturnFalse_WhenNotFoundUser()
+    {
+        // Arrange
+        string userName = "valid@3ashkxlmas";
+        var sut = new GithubServices(_githubSearch);
+        // Action
+        var result = await sut.GetRepos(userName);
+        //Assert
+        result.IsSuccess.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task GetRepo_ShouldVerifyGithubSearch_WhenIsCalled()
+    {
+        // Arrange
+        string userName = "kxlmas";
+        var githubSearch = Substitute.For<IGithubSearch>();
+        var sut = new GithubServices(githubSearch);
+        // Action
+        var result = await sut.GetRepos(userName);
+        await githubSearch.Received(1).GetUserRepositories(userName);
+    }
+}
